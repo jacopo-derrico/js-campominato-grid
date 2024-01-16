edd269baa4fb56adbe0697095f60ea4f8acec2c3
jacopo-derrico <derricoj.design@gmail.com>
2024-01-16 17:06:28 +0100

    added difficulty selector w/ different grids

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,48 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
 // - fare 3 classi per la dimensione celle con nuovo calc .box-easy .box-medium . box-hard
 let playBtn = document.getElementById('playBtn')
 let gridHTML = document.getElementById('grid')
+let difficultySelection = document.getElementById('difficulty')
 
-function generateGrid() {
-    for (let i = 1; i <= 100; i++) {
+gridHTML.innerHTML = "<h2>Scegli una difficoltà e comincia a giocare!"
+
+
+function generateGrid(number, name) {
+    for (let i = 1; i <= number; i++) {
         let box = document.createElement('div')
-        
-        box.classList.add('box')
-    
+
+        box.classList.add('box', name)
+
         box.innerHTML = `<span>${[i]}</span>`
-    
-        box.addEventListener('click', function(){
+
+        box.addEventListener('click', function () {
             this.classList.toggle('box-active')
             console.log(`Cella numero: ${[i]}`)
         })
-        
+
         gridHTML.append(box)
     }
 }
 
-playBtn.addEventListener('click', generateGrid)
+playBtn.addEventListener('click', function () {
 
+    gridHTML.innerHTML = ""
 
+    let difficultyValue = difficultySelection.value
 
+    if (difficultyValue === 'easy') {
+        return generateGrid(100, 'box-easy')
+    } else if (difficultyValue === 'medium') {
+        return generateGrid(81, 'box-medium')
+    } else {
+        return generateGrid(49, 'box-hard')
+    }
+})
